8c556077020db76828b20b69b3fa43efbd57f151
Liplum <Li_plum@outlook.com>
2022-12-19 09:43:11 +0800

    SoundFile outline.

--- lib/foundation.dart
+++ lib/foundation.dart
@@ -1,2 +1,6 @@
+import 'dart:ui';
+
+import 'package:audioplayers/audioplayers.dart';
+
 part 'foundation/soundpack.dart';
 part 'foundation/music.dart';
--- lib/foundation/soundpack.dart
+++ lib/foundation/soundpack.dart
@@ -1,5 +1,31 @@
 part of '../foundation.dart';
 
-class SoundFile {
+abstract class SoundFile {
+  Future<void> loadInto(AudioPlayer player);
+}
+
+class AssetSoundFile implements SoundFile {
+  final String path;
+
+  const AssetSoundFile(this.path);
+
+  @override
+  Future<void> loadInto(AudioPlayer player) async {
+    await player.setSourceAsset(path);
+  }
+}
+
+class LocalSoundFile implements SoundFile {
+  final String path;
+
+  const LocalSoundFile(this.path);
+
+  @override
+  Future<void> loadInto(AudioPlayer player) async {
+    await player.setSourceDeviceFile(path);
+  }
+}
+
+class SoundPack {
 
 }
